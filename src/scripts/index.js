dc0acd706565c07f29ab1dc1ddfca6dd2a457172
ErikaDelaRosa <erika-delarosa@hotmail.com>
2020-04-23 13:35:36 +0200

    daily exercises

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
-const addToList = () => {
+//erika version: 
+/*const addToList = () => {
     let userInput = document.querySelector('#userText').value;
     //console.log(userInput);
     let newLi = document.createElement("li");
@@ -7,7 +8,33 @@ const addToList = () => {
     document.querySelector('.result').appendChild(newLi);
     document.querySelector('#userText').value="";
 };
+*/
+//Hadi version:
+const addToList = () => {
+  let userInput = document.querySelector("#userText").value;
+  if (userInput == "") {
+    return alert("Write something Please");
+  }
+  let newLi = document.createElement("li");
+  let textNode = document.createTextNode(userInput);
+  newLi.appendChild(textNode);//i add only text, and its textNode to make it works
+  document.querySelector(".result").appendChild(newLi);
+  document.querySelector("#userText").value = "";
+  let list = document.querySelectorAll("li:nth-child(odd)");
+  list.forEach(({ style: item }) => {
+    item.backgroundColor = colorGen();
+  });
+  let span = document.createElement("span");
+  let text = document.createTextNode("x");
+  span.className = "close";
+  span.appendChild(text);
+  let listAll = document.querySelectorAll("li");
+  listAll.forEach((item) => {
+    item.appendChild(span);
+  });
+};
 
+//erika version
 function colorGen() {
     let result = "#";
     let HexNum = "0123456789ABCDEF";
@@ -15,15 +42,38 @@ function colorGen() {
       result += HexNum[Math.floor(Math.random() * 16)];
       // console.log(result);
     }
-    result+= "50";
+    result += "50";
     return result;
   };
+
+  let span = document.createElement("span");
+  let text = document.createTextNode("x");
+  span.className = "close";
+  span.appendChild(text);
+  let listAll = document.querySelectorAll("li");
+  listAll.forEach((item) => {
+    item.appendChild(span);
+  });
+  
+  //hadi version
+/*const colorGen = () => {
+  let result = "#",
+    HexString = "0123456789ABCDEF";
+  for (let i = 0; i < 6; i++) {
+    result += HexString[Math.floor(Math.random() * 16)];
+  }
+  result += "50";
+  return result;
+};
+
+
 /*
   completeList.forEach((element, index) => { 
      if (index % 2 !== 0) { 
     element.style.backgroundColor = colorGen();
   };
 */
+
 let completeList = document.querySelectorAll(".result");
 completeList.forEach(({style: item},i) => {
     if (i % 2 ==1){
